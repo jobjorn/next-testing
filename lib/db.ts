d5432a5e475e6d14423c79859b1d44f7f8671151
Jobjörn Folkesson <jobjorn@jobjorn.se>
2021-05-18 21:19:27 +0200

    ytterligare databasanslutningstest

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,5 @@
 import ServerlessClient from 'serverless-postgres';
+//import { Client } from 'pg';
 
 export const client = new ServerlessClient({
   connectionString: process.env.DB_CONNECTION_STRING,
