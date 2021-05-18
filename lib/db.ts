e2f2fdfe71e6957cd49fa43525530dc9ea5da959
Jobjörn Folkesson <jobjorn@jobjorn.se>
2021-05-18 20:53:43 +0200

    testar lite databas

---
+++ lib/db.ts
@@ -0,0 +1,19 @@
+import ServerlessClient from 'serverless-postgres';
+
+export const client = new ServerlessClient({
+  connectionString: process.env.DB_CONNECTION_STRING
+});
+
+export async function query(
+  q: string,
+  values: (string | number)[] | string | number = []
+) {
+  try {
+    await client.connect();
+    const results = await client.query(q, values);
+    await client.clean();
+    return results;
+  } catch (e) {
+    throw Error(e.message);
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
-import { NextPage } from 'next';
+import { query } from 'lib/db';
+import { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 
-const IndexPage: NextPage<{}> = ({}) => {
+interface Props {
+  test: string;
+}
+
+const IndexPage: NextPage<Props> = ({ test }) => {
   return (
     <div>
       <Head>
         <title>NextJS Typescript Starter</title>
       </Head>
       <p>Just a starter for creating NextJS Typescript apps.</p>
+      <p>{test}</p>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const result = await query('SELECT NOW() as now');
+  console.log(result);
+  return { props: { test: 'hej3' } };
 };
 
 export default IndexPage;
